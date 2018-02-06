d673db12238f92c67b5bb5bd460b3a8190d404b3
voyager.woo <voyager.woo@gmail.com>
2018-02-07 06:48:41 +0900

    first commit

---
+++ mnist_dataset/download_mnist_dataset.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget http://www.pjreddie.com/media/files/mnist_train.csv
+wget http://www.pjreddie.com/media/files/mnist_test.csv
+wget https://raw.githubusercontent.com/makeyourownneuralnetwork/makeyourownneuralnetwork/master/mnist_dataset/mnist_train_100.csv
+wget https://raw.githubusercontent.com/makeyourownneuralnetwork/makeyourownneuralnetwork/master/mnist_dataset/mnist_test_10.csv
